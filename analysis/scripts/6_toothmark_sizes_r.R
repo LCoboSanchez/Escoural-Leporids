fd934f30d69a456cb1fe7dd63f22ab3a2ffee08e
jmcascalheira <jmcasca@gmail.com>
2024-09-16 10:14:36 +0100

    replied to reviewers

---
+++ analysis/scripts/6_toothmark_sizes_r.R
@@ -0,0 +1,43 @@
+#Alternating carnivore and Neanderthal activities at Escoural Cave: 
+#Insights from the taphonomic and machine learning analysis of leporid remains
+
+# Cobo-Sánchez, L.; Rufà, A.; Cascalheira, J.
+
+# Bone surface modifications. Mean and CI plot for pits and puncture breadth
+
+library(DescTools)
+library(ggplot2)
+data<-read.csv("6_pits_punct_Esc.csv", sep=";", header=TRUE)
+data <- as.numeric(unlist(data))
+
+# Calculate the mean and 95% confidence interval
+result <- t.test(data, conf.level = 0.95)
+
+# Extract the mean and confidence interval
+mean_value <- mean(data)
+ci_lower <- result$conf.int[1]
+ci_upper <- result$conf.int[2]
+
+# Print the results
+mean_value
+ci_lower
+ci_upper
+
+data_all<-read.csv("6_pits_punct_all_rr.csv",sep=";",header=TRUE)
+
+# Create the plot
+plot<-ggplot(data_all, aes(x = Group, y = Mean)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = CI_lower, ymax = CI_upper), width = 0.2) +
+  theme_minimal() +
+  labs(title = "",
+       x = "Group",
+       y = "Mean") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+  panel.background = element_rect(fill = "white", color = NA),
+  plot.background = element_rect(fill = "white", color = NA)
+)
+
+# Save the plot with 300 dpi resolution
+ggsave("Figure9r.png", plot = plot, width = 10, height = 8, dpi = 300)
+
---
+++ analysis/scripts/7_HCPC_kmeans_tapho_rr.R
@@ -0,0 +1,107 @@
+#Alternating carnivore and Neanderthal activities at Escoural Cave: 
+#Insights from the taphonomic and machine learning analysis of leporid remains
+
+# Cobo-Sánchez, L.; Rufà, A.; Cascalheira, J.
+
+# 7. Taphonomy HCPC and kmeans cluster analysis
+
+# Load necessary libraries
+library(FactoMineR)
+library(factoextra)
+library(missForest)
+library(ggrepel)
+library(cluster)
+library(fpc)
+
+# Load data
+data <- read.csv("7_HCPC_Kmeans_tapho_rr.csv", sep = ";", na.strings = "na")
+
+# HCPC
+# Impute missing values
+set.seed(123)
+data_imputed <- missForest(data[, -1])$ximp  # Exclude the first column if it's not numeric
+
+# PCA
+pca_result <- PCA(data_imputed, graph = TRUE)
+
+# Perform HCPC
+hcpc_result <- HCPC(pca_result, graph = FALSE)
+hcpc_result <- HCPC(pca_result, graph = TRUE)
+
+# Plot HCPC dendrogram
+fviz_dend(hcpc_result, cex = 0.7, palette = "futurama", rect = TRUE, rect_fill = TRUE, rect_border = "jco",
+          labels_track_height = 0.8)
+
+# Create the factor map plot
+factor_map <- fviz_cluster(hcpc_result, 
+                           geom = "point", 
+                           ellipse.type = "convex", 
+                           palette = "Set2", 
+                           ggtheme = theme_minimal(),
+                           shape = 19
+)
+
+# Add sample names to the plot
+factor_map <- factor_map + 
+  geom_text_repel(aes(label = data$Assemblage), 
+                  vjust = -1, 
+                  hjust = 0.5, 
+                  size = 2.5) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    panel.background = element_rect(fill = "white", color = NA),
+    plot.background = element_rect(fill = "white", color = NA)
+  )
+
+# Save the plot with 300 dpi resolution
+ggsave("Figure10_rr.png", plot = factor_map, width = 10, height = 8, dpi = 300)
+
+
+# Convert clustering labels to numeric
+clustering_labels <- as.numeric(hcpc_result$data.clust$clust)
+
+# Calculate silhouette scores
+silhouette_score <- silhouette(clustering_labels, dist(data_imputed))
+
+# Plot silhouette scores
+fviz_silhouette(silhouette_score)
+
+
+# K- means clustering analysis
+
+# Perform k-means clustering with a chosen number of clusters 
+set.seed(123)
+kmeans_result <- kmeans(data_imputed, centers = 6)
+
+# Print cluster centers
+print(kmeans_result$centers)
+
+# Plot the clusters
+fviz_cluster(kmeans_result, data = data_imputed)
+
+factor_map <- fviz_cluster(kmeans_result, data=data_imputed, geom = "point", ellipse.type = "convex", palette = "Set2", ggtheme = theme_minimal(),shape=19)
+factor_map <- factor_map + geom_text_repel(aes(label = data$Assemblage), vjust = -1, hjust = 0.5, size = 2.5)
+factor_map <- factor_map + theme(
+  axis.text.x = element_text(angle = 45, hjust = 1),
+  panel.background = element_rect(fill = "white", color = NA),
+  plot.background = element_rect(fill = "white", color = NA)
+)
+
+plot(factor_map)
+# Save the plot with 300 dpi resolution
+ggsave("Figure11_rr.png", plot = factor_map, width = 10, height = 8, dpi = 300)
+
+# Convert clustering labels to numeric
+clustering_labels <- as.numeric(kmeans_result$data.clust$clust)
+
+# Calculate silhouette scores
+silhouette_scores <- silhouette(kmeans_result$cluster, dist(data_imputed))
+
+# Plot silhouette scores
+fviz_silhouette(silhouette_scores)
+
+write.table(silhouette_scores,"silhouette_scores_kmeans_rr.csv",sep=";")
+write.table(kmeans_result$centers,"kmeans_result_centers_rr.csv",sep=";")
+
+
+
